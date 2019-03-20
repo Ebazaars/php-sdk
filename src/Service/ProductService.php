8ba3a9236d6276135bff50c2a3f774a64dcaf07e
ebazaars <github@ebazaars.net>
2019-03-20 06:49:20 +0300

    Update product service

--- src/Service/ProductService.php
+++ src/Service/ProductService.php
@@ -7,12 +7,20 @@ namespace EbazaarsSdk\Service;
 class ProductService extends AbstractService
 {
     const ALL_PRODUCTS = '/products/';
+    const GET_BY_SLUG = '/product/slug/{slug}';
 
     public function getAllProducts()
     {
         $response = $this->getClient()->getRequest(self::ALL_PRODUCTS);
 
         return $this->getContent($response);
+    }
+
+    public function getBySlug($slug)
+    {
+        $response = $this->getClient()->getRequest(str_replace('{slug}', $slug, self::GET_BY_SLUG));
+
+        return $this->getClient($response);
     }
 
 }
