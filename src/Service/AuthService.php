e33ed10d4f3d6135c587199e4df050b04de140da
ebazaars <github@ebazaars.net>
2019-05-10 00:30:01 +0300

    token is valid

--- src/Service/AuthService.php
+++ src/Service/AuthService.php
@@ -2,16 +2,29 @@
 
 namespace EbazaarsSdk\Service;
 
+use EbazaarsSdk\Authentication\Token;
+
 class AuthService extends AbstractService
 {
 
     const AUTH = '/auth/';
+    const IS_VALID = '/token/is_valid/';
 
     public function getToken($username, $password)
     {
         $response = $this->getClient()->postRequest(
             self::AUTH,
             ['form_params' => ['username' => $username, 'password' => $password]]
+        );
+
+        return $this->getContent($response);
+    }
+
+    public function tokenIsValid(Token $token)
+    {
+        $response = $this->getClient()->postRequest(
+            self::IS_VALID,
+            ['form_params' => ['token' => $token->getToken()]]
         );
 
         return $this->getContent($response);
