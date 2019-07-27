e72dd183b614c5921dfeffa297be9f3bb721b392
ebazaars <github@ebazaars.net>
2019-07-27 09:57:58 +0300

    Create get by cookie method for order

--- src/Service/OrderService.php
+++ src/Service/OrderService.php
@@ -10,6 +10,7 @@ class OrderService extends AbstractService
 {
 
     const ORDER_CREATE = '/order/create';
+    const GET_ORDER_BY_COOKIE = '/order/cookie/{order_cookie}';
 
     public function create(Order $order, $options = [])
     {
@@ -20,4 +21,13 @@ class OrderService extends AbstractService
         return $this->getContent($response);
     }
 
+    public function getByCookie($cookie, $options = [])
+    {
+        $order = $this->getClient()->getRequest(
+            str_replace('{order_cookie}', $cookie, self::GET_ORDER_BY_COOKIE),
+            $options
+        );
+
+        return $this->getContent($order);
+    }
 }
