f869d8fb625fbd00940ab79bcc297571f9cffdf4
ebazaars <github@ebazaars.net>
2019-10-07 07:10:39 +0300

    Payment pay

--- src/Service/PaymentService.php
+++ src/Service/PaymentService.php
@@ -11,6 +11,7 @@ class PaymentService extends AbstractService
 
     const PAYMENT_CREATE = '/payment/create';
     const CHECK_PAYMENT = '/payment/check/{orderUuid}';
+    const PAYMENT_PAY = '/pay/';
 
     public function create(Order $order, $completeUrl = '', $options = [])
     {
@@ -29,6 +30,15 @@ class PaymentService extends AbstractService
             str_replace('{orderUuid}', $orderUuid, self::CHECK_PAYMENT),
             $options
         );
+
+        return $this->getContent($response);
+    }
+
+    public function pay(Order $order, $completeUrl = '', $options = [])
+    {
+        $options['form_params'] = json_decode(json_encode($order), true);
+        $options['form_params']['complete_url'] = $completeUrl;
+        $response = $this->getClient()->postRequest(self::PAYMENT_PAY, $options);
 
         return $this->getContent($response);
     }
