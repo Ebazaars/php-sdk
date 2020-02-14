1a1933d4dccd8e2e16b4787412597b7496c97c7b
ebazaars <github@ebazaars.net>
2020-02-14 06:11:06 +0300

    All pages with pagination

--- src/Service/PageService.php
+++ src/Service/PageService.php
@@ -9,6 +9,7 @@ class PageService extends AbstractService
 
     const PAGES = '/pages/';
     const BY_SLUG = '/page/slug/{slug}';
+    const GET_ALL_PAGE_WITH_PAGINATION = '/page/all/page/{page}';
     const GET_ALL_TAG = '/tag/all';
 
     public function getAll($options = [])
@@ -28,6 +29,13 @@ class PageService extends AbstractService
     public function getAllTags($options = [])
     {
         $response = $this->getClient()->getRequest(self::GET_ALL_TAG, $options);
+
+        return $this->getContent($response);
+    }
+
+    public function getAllWithPagination($page = 1, $options = [])
+    {
+        $response = $this->getClient()->getRequest(str_replace('{page}', $page, self::GET_ALL_PAGE_WITH_PAGINATION));
 
         return $this->getContent($response);
     }
