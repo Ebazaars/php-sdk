73fc6f7556162e4994d7ad624b2aae8bd7be7c31
ebazaars <github@ebazaars.net>
2020-02-15 12:13:25 +0300

    Filter page by tags with pagination

--- src/Service/PageService.php
+++ src/Service/PageService.php
@@ -11,6 +11,7 @@ class PageService extends AbstractService
     const BY_SLUG = '/page/slug/{slug}';
     const GET_ALL_PAGE_WITH_PAGINATION = '/page/all/page/{page}';
     const GET_ALL_TAG = '/page/tag/all';
+    const FILTER_BY_TAGS_WITH_PAGINATION = '/page/filter/by/tags/page/{page}';
 
     public function getAll($options = [])
     {
@@ -35,9 +36,19 @@ class PageService extends AbstractService
 
     public function getAllWithPagination($page = 1, $options = [])
     {
-        $response = $this->getClient()->getRequest(str_replace('{page}', $page, self::GET_ALL_PAGE_WITH_PAGINATION), $options);
+        $response = $this->getClient()->getRequest(
+            str_replace('{page}', $page, self::GET_ALL_PAGE_WITH_PAGINATION),
+            $options
+        );
 
         return $this->getContent($response);
     }
 
+    public function filterByTags($tags, $page = 1, $options = [])
+    {
+        $options['form_params']['tags'] = $tags;
+        $response = $this->getClient()->postRequest(str_replace('{page}', $page, self::FILTER_BY_TAGS_WITH_PAGINATION), $options);
+
+        return $this->getContent($response);
+    }
 }
