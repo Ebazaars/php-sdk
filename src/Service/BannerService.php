03d6bb8ad1e032c05bf85d35651a55f06f5a7a22
ebazaars <github@ebazaars.net>
2020-04-02 14:07:30 +0300

    Banner filter integration

--- src/Service/BannerService.php
+++ src/Service/BannerService.php
@@ -4,14 +4,25 @@
 namespace EbazaarsSdk\Service;
 
 
+use EbazaarsSdk\Holder\Banner\FilterHolder;
+
 class BannerService extends AbstractService
 {
 
     const GET_BY_BANNER_TYPE = '/banner/type/{slug}';
+    const FILTER = '/banner/filte';
 
     public function getByBannerType($slug, $options = [])
     {
         $response = $this->getClient()->getRequest(str_replace('{slug}', $slug, self::GET_BY_BANNER_TYPE), $options);
+
+        return $this->getContent($response);
+    }
+
+    public function filter(FilterHolder $filter, $options = [])
+    {
+        $options['form_params'] = json_decode(json_encode($filter), true);
+        $response = $this->getClient()->postRequest(self::FILTER, $options);
 
         return $this->getContent($response);
     }
